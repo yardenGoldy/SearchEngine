aa22dc5d5f4e599b9fc62856ac2cd2d96cb0280f
yuval <yuvi.zehavi@gmail.com>
2018-11-21 02:05:53 +0200

    add percent and range.

--- src/main/java/RangeToken.java
+++ src/main/java/RangeToken.java
@@ -1,8 +1,29 @@
 import java.util.List;
 
-public class RangeToken implements IToken {
+public class RangeToken extends NumberToken {
+
     @Override
     public ParsedResult TryParse(List<String> sentence) {
-        return null;
+        String first = sentence.get(0);
+        StringBuilder result = new StringBuilder();
+        Integer index = 0;
+        if (!first.equals("Between")) {
+            return null;
+        } else {
+            index++;
+            ParsedResult firstNumber = super.TryParse(sentence.subList(1, sentence.size()));
+            if(firstNumber.IsMatch && sentence.get(firstNumber.Index + 1).equals("and"))
+            {
+                index += firstNumber.Index + 1;
+                ParsedResult secondNumber = super.TryParse(sentence.subList(firstNumber.Index + 2, sentence.size()));
+                if(secondNumber.IsMatch)
+                {
+                    index += secondNumber.Index;
+                    result.append(String.format("%s-%s", firstNumber.ParsedSentence, secondNumber.ParsedSentence));
+                }
+            }
+        }
+
+        return new ParsedResult(true, result, index);
     }
 }
