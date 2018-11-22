8876c140b3290eddb4f5abe2a298ea0fdbad3b46
eranedri <eranedrimail@gmail.com>
2018-11-22 13:38:28 +0200

    Default Changelist

--- src/main/java/Model/Main.java
+++ src/main/java/Model/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
 
     {
         try {
-            searchEngine = new SearchEngine("/Users/eranedri/IdeaProjects/Model.SearchEngine-Part1/Eran_Corpuse","/Users/eranedri/IdeaProjects/Model.SearchEngine-Part1/stop_words.txt",false);
+            searchEngine = new SearchEngine("/Users/eranedri/IdeaProjects/SearchEngine/MIMIcorpus","/Users/eranedri/IdeaProjects/SearchEngine/stop_words.txt",false);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -33,17 +33,17 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
-        ReadFile rd = new ReadFile();
-        try
-        {
-            HashMap<String,DocDetailes> yarden = rd.ReadAllDocs("C:\\Git\\Model.SearchEngine\\corpus");
-            Parse parse = new Parse();
-            parse.ParseCorpus(new ArrayList<DocDetailes>(yarden.values()), false, null);
-        }
-        catch (IOException e)
-        {
-            System.out.print("kine");
-        }
+//        ReadFile rd = new ReadFile();
+//        try
+//        {
+//            HashMap<String,DocDetailes> yarden = rd.ReadAllDocs("C:\\Git\\Model.SearchEngine\\MINIcorpus");
+//            Parse parse = new Parse();
+//            parse.ParseCorpus(new ArrayList<DocDetailes>(yarden.values()), false, null);
+//        }
+//        catch (IOException e)
+//        {
+//            System.out.print("kine");
+//        }
     }
 }
 
